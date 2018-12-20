bc32896847a6fbc58aa1e34db21f6ba7dec8af4d
Tim Hehmann <tim.hehmann@googlemail.com>
2018-12-20 14:26:35 +0100

    define basic functions for Dartboard and declare what needs to happen in these

--- DartMania/Dartboard.swift
+++ DartMania/Dartboard.swift
@@ -50,55 +50,35 @@ class Dartboard {
  
     }
     
+    func addField (points: Int) {
+        // - look in array what the position of the points is
+        // - according to that position get the right starting and end angles for the arc
+        // - create 2x field
+        // - create outer 1x field
+        // - create 3x field
+        // - create inner 1x field
+        // if bullseye create dots..
+    }
     
-//    func createDartboardElements () {
-//
-//        let circlePath = UIBezierPath(ovalIn: CGRect(x: 200, y: 200, width: 150, height: 150))
-//        var segments: [CAShapeLayer] = []
-//        //let segmentAngle: CGFloat = (360 * 0.125) / 360
-//        let segmentAngle: CGFloat = CGFloat(1 / numberOfFields)
-//
-//        //create bulls eye
-//        //create second bulls eye
-//        //create outer edge
-//
-//        for i in 0..<numberOfFields {
-//
-//            // create inner -> 1x
-//            let inner1x = SKNode()
-//            let circle = SKShapeNode()
-//            let circleLayer = CAShapeLayer()
-//            circleLayer.path = circlePath.cgPath
-//
-//
-//            // start angle is number of segments * the segment angle
-//            circleLayer.strokeStart = segmentAngle * CGFloat(i)
-//
-//            // end angle is the start plus one segment, minus a little to make a gap
-//            // you'll have to play with this value to get it to look right at the size you need
-//            let gapSize: CGFloat = 0.008
-//            circleLayer.strokeEnd = circleLayer.strokeStart + segmentAngle - gapSize
-//
-//            circleLayer.lineWidth = 10
-//            circleLayer.strokeColor = UIColor(red:0,  green:0.004,  blue:0.549, alpha:1).cgColor
-//            circleLayer.fillColor = UIColor.clear.cgColor
-//
-//            // add the segment to the segments array and to the view
-//            segments.append(circleLayer)
-//            // self.layer.addSublayer(segments[i])
-//
-//
-//
-//
-//
-//            // create 3x
-//            let trippleField = SKShapeNode()
-//            // create outer -> 1x
-//            let outer1x = SKShapeNode()
-//            // create double
-//            let doubleField = SKShapeNode()
+    func addSingleField (points: Int, radius: CGFloat, startAngle: CGFloat, endAngle: CGFloat, color: UIColor) {
+        // var element = DartboardElement (points: points)
+        //        let outerArc = SKShapeNode()
+        //        let path = UIBezierPath(arcCenter: CGPoint(x: 100, y: 200), radius: 30, startAngle: 0, endAngle: 2*CGFloat.pi, clockwise: true).cgPath
+        //        outerArc.path = path
+        //        outerArc.fillColor = UIColor.red
+        //        outerArc.strokeColor = UIColor.red
+        //        gameScene?.addChild(outerArc)
+        // gameScene.addChild(element.node)
+    }
+    
+    
+    func createDartboardElements () {
+
+//        for points in pointsArray {
+//            addField(points)
 //        }
-//    }
+        
+    }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
