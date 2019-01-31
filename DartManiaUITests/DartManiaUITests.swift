10259cfa212b4c0103ebb082ee5782a7c7fe550c
Tim Hehmann <tim.hehmann@googlemail.com>
2019-01-31 19:43:43 +0100

    setup environment for ui tests

--- DartManiaUITests/DartManiaUITests.swift
+++ DartManiaUITests/DartManiaUITests.swift
@@ -1,8 +1,8 @@
 //
 //  DartManiaUITests.swift
-//  DartManiaTests
+//  DartManiaUITests
 //
-//  Created by Tim Hehmann on 17.01.19.
+//  Created by Tim Hehmann on 31.01.19.
 //  Copyright © 2019 Tim Hehmann. All rights reserved.
 //
 
@@ -10,29 +10,29 @@ import XCTest
 
 class DartManiaUITests: XCTestCase {
     var app: XCUIApplication!
-
+    
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
-
+        
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-
+        
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
         app = XCUIApplication()
         app.launch()
-
+        
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
-
+    
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
+    
     func testExample() {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
         app.buttons["Join game"].tap()
-        //XCTAssertTrue(app.otherElements["dashboardView"].exists);
+        XCTAssertTrue(app.otherElements["dashboardView"].exists);
     }
 
 }
