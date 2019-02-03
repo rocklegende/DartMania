026f87672a8a9decd15c618a5c1d0308b9de0a99
Tim Hehmann <tim.hehmann@googlemail.com>
2019-02-03 12:34:16 +0100

    fix stuff

--- DartManiaUITests/DartManiaUITests.swift
+++ DartManiaUITests/DartManiaUITests.swift
@@ -28,11 +28,19 @@ class DartManiaUITests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
     
-    func testExample() {
+    func testClickOnStartOpensGameSettings() {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-        app.buttons["Join game"].tap()
-        XCTAssertTrue(app.otherElements["dashboardView"].exists);
+        app.buttons["Start a game!"].tap()
+        XCTAssertTrue(app.otherElements["gameSettingsView"].exists);
+    }
+    
+    func testClickOnStartLocalGameOpensGame() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        app.buttons["Start a game!"].tap()
+        app.buttons["Start local game"].tap()
+        XCTAssertTrue(app.otherElements["gameView"].exists);
     }
 
 }
